a96ea2ef2888c77629d1d52d1b2d357c3407fa35
Dhanush Gc <47391913+dhanushgc@users.noreply.github.com>
2020-02-26 14:43:35 +0530

    Update Color Filtering OpenCV.py

--- opencv/Color Filtering OpenCV.py
+++ opencv/Color Filtering OpenCV.py
@@ -7,6 +7,8 @@ while(1):
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
+    edges = cv2.Canny(frame,100,200)
+    
     lower_red = np.array([150,150,30])
     upper_red = np.array([255,255,180])
     
@@ -14,8 +16,9 @@ while(1):
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
     cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
+    #cv2.imshow('mask',mask)
     cv2.imshow('res',res)
+    cv2.imshow('EDGE',edges)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
